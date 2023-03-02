8eecc52371b0dd5e8a7c7ed793d67d539db083f3
Tringapps-Jeevanandam <jeevanandam.kathiresan.tringapps.com>
2023-03-02 15:42:28 +0530

    a

---
+++ src/main/java/project/student/STUDENT.java
@@ -0,0 +1,34 @@
+package project.student;
+
+
+public class STUDENT {
+    String name;
+    int age;
+    double gpa;
+
+    STUDENT(String name, int age, double gpa){
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    protected void setName(String name){
+        this.name = name;
+    }
+    protected void setAge(int age){
+        this.age = age;
+    }
+    protected void setGpa(double gpa){
+        this.gpa = gpa;
+    }
+    
+    protected String getName(){
+        return name;
+    }
+    protected String getAge(){
+        return Integer.toString(age);
+    }
+    protected String getGpa(){
+        return Double.toString(gpa);
+    }
+}
